936dab711b87241966d10bc7a2af76c4bdc45d3f
Eric Sanchez <ericsanchez@outlook.com>
2015-09-29 16:01:49 -0400

    ...

--- TalkinToTheNet/TalkinToTheNet/AppDelegate.m
+++ TalkinToTheNet/TalkinToTheNet/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "FourSquareTableViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    FourSquareTableViewController* fs = [[FourSquareTableViewController alloc] init];
+    [fs makeVenueRequest];
     return YES;
 }
 
